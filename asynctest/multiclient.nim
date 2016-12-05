df72b47c93b0380c3223827c48082c5b2f6560fd
Vadim Kilibaba <vkulibaba@surfy.ru>
2016-12-05 19:56:03 +0300

    tests

--- asynctest/multiclient.nim
+++ asynctest/multiclient.nim
@@ -1,4 +1,4 @@
-import net, threadpool
+import net, threadpool, times
 {.experimental.}
 
 const NL = chr(13) & chr(10)
@@ -6,19 +6,21 @@ const NL = chr(13) & chr(10)
 proc newTask(task:int) = 
   var socket = newSocket()
   socket.connect("127.0.0.1",Port(11213))
-  for i in 1..3:
+  for i in 1..1000000:
     let resp = $task & ":" & $i & NL
     socket.send(resp)
-    let res = socket.recv(8)
-    echo $resp,$res
+    let res = socket.recv(800)
+    #echo $resp,$res
   socket.send("quit")
   socket.close()
 
 proc main()=
   echo "start"
+  var t = toSeconds(getTime())
   parallel:
-    for i in 1..5:
+    for i in 1..1:
       spawn newTask(i)
   echo "end"
+  echo "Read time [s] ", $(toSeconds(getTime()) - t)    
 
 main()
