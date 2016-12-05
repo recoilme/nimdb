5c895d2c125c80bf12e890fe4cf93ec21da4e8b9
Vadim Kilibaba <vkulibaba@surfy.ru>
2016-12-05 19:37:16 +0300

    close conn

--- asynctest/multiclient.nim
+++ asynctest/multiclient.nim
@@ -4,19 +4,20 @@ import net, threadpool
 const NL = chr(13) & chr(10)
 
 proc newTask(task:int) = 
-  let socket = newSocket()
+  var socket = newSocket()
   socket.connect("127.0.0.1",Port(11213))
   for i in 1..3:
     let resp = $task & ":" & $i & NL
     socket.send(resp)
     let res = socket.recv(8)
     echo $resp,$res
-  #socket.send("quit")
+  socket.send("quit")
+  socket.close()
 
 proc main()=
   echo "start"
   parallel:
-    for i in 1..2:
+    for i in 1..5:
       spawn newTask(i)
   echo "end"
 
